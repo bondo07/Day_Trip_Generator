c8772f251cd475a97e6c496c3f3b337f1bb647a5
Armando Beltran <armando_b@live.com>
2022-09-16 14:17:24 -0500

    Created list fun to print rand.choice

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
 import random
-def projectgreeting():
-    print("Armando's Day Trip Generator")
+# def projectgreeting():
+#     print("Armando's Day Trip Generator")
 
-projectgreeting()
+# projectgreeting()
 
-def dest():
-    destinations = ["Gondor", "The Death Star", "Jurassic Park", "Willy Wonka's Chocolate Factory", 
-        "Hogwarts School of Witchcraft & Wizadry", "Stanly Hotel", "King's Landing", "Gotham City"]
-def rest():
-    restaurants = ["Bob's Burgers", "The Three BroomSticks", "The Mos Eisley Cantina", "The Krusty Crab",
-     "Pizza Planet", "La Ratatouille", "Los Pollos Hermanos", "Gus's Galaxy Grill"]
+# def dest():
+#     destinations = ["Gondor", "The Death Star", "Jurassic Park", "Willy Wonka's Chocolate Factory", 
+#         "Hogwarts School of Witchcraft & Wizadry", "Stanly Hotel", "King's Landing", "Gotham City"]
+#     chosen_d = random.choice(destinations)
+#     destination = ""
+#     if destination in chosen_d:
+#             print(chosen_d)
+# dest()
+
+# def rest():
+#     restaurants = ["Bob's Burgers", "The Three BroomSticks", "The Mos Eisley Cantina", "The Krusty Crab",
+#      "Pizza Planet", "La Ratatouille", "Los Pollos Hermanos", "Gus's Galaxy Grill"]
+#     chosen_r = random.choice(restaurants)
+#     restaraunt = ""
+#     if restaraunt in chosen_r:
+#         print(chosen_r)
+
+# rest()
+
 def ent():
     entertainment_opt = ["Learning Magic", "Running for your life", "Battling Jedi's", "Ruling a Kingdom",
     "Becoming a hero", "Hunting big lizards", "Going on a Carnival Ride", "Live Crowning event of new King"]
+    chosen_e = random.choice(entertainment_opt)
+    entertainment = ""
+    if entertainment in chosen_e:
+        print(chosen_e)
+ent()
