8a4d3002abc5e1422413011972f44c051aabb4c5
Armando Beltran <armando_b@live.com>
2022-09-16 13:30:17 -0500

    created lists under fun

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+import random
 def projectgreeting():
     print("Armando's Day Trip Generator")
 
 projectgreeting()
+
+def dest():
+    destinations = ["Gondor", "The Death Star", "Jurassic Park", "Willy Wonka's Chocolate Factory", 
+        "Hogwarts School of Witchcraft & Wizadry", "Stanly Hotel", "King's Landing", "Gotham City"]
+def rest():
+    restaurants = ["Bob's Burgers", "The Three BroomSticks", "The Mos Eisley Cantina", "The Krusty Crab",
+     "Pizza Planet", "La Ratatouille", "Los Pollos Hermanos", "Gus's Galaxy Grill"]
+def ent():
+    entertainment_opt = ["Learning Magic", "Running for your life", "Battling Jedi's", "Ruling a Kingdom",
+    "Becoming a hero", "Hunting big lizards", "Going on a Carnival Ride", "Live Crowning event of new King"]
