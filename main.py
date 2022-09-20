cf558508cbb70208a8ae77b235d78c62721d712c
Armando Beltran <armando_b@live.com>
2022-09-19 22:33:48 -0500

    Final project: complete ez-mode

--- main.py
+++ main.py
@@ -1,74 +1,54 @@
 import random
-def projectgreeting():
-    print("Armando's Day Trip Generator")
 
-projectgreeting()
-
-# def place():
-#     destinations = ["Gondor", "The Death Star", "Jurassic Park", "Willy Wonka's Chocolate Factory", 
-#         "Hogwarts School of Witchcraft & Wizadry", "Stanly Hotel", "King's Landing", "Gotham City"]
-#     chosen_d = random.choice(destinations)
-#     destination = ""
-#     if destination in chosen_d:
-#             print(chosen_d)
-# # place()
-
-# def food():
-#     restaurants = ["Bob's Burgers", "The Three BroomSticks", "The Mos Eisley Cantina", "The Krusty Crab",
-#      "Pizza Planet", "La Ratatouille", "Los Pollos Hermanos", "Gus's Galaxy Grill"]
-#     chosen_r = random.choice(restaurants)
-#     restaraunt = ""
-#     if restaraunt in chosen_r:
-#         print(chosen_r)
-
-# # food()
-
-# def entertainment():
-#     entertainment_opt = ["Learning Magic", "Running for your life", "Battling Jedi's", "Ruling a Kingdom",
-#     "Becoming a hero", "Hunting big lizards", "Carnival Ride", "Live Crowning event of new King"]
-#     chosen_e = random.choice(entertainment_opt)
-#     entertainments = ""
-#     if entertainments in chosen_e:
-#         print(chosen_e)
-# # enterainment()
-
-# def transport():
-#     mode_of_transport = ["Flying on giant Eagles", "Millenium Falcon flight", "Jeep", "River Boat",
-#     "Broomstick", "Teleportation", "Horseback drawn carriage", "Batmobile"]
-#     chosen_t = random.choice(mode_of_transport)
-#     transportation = ""
-#     if transportation in chosen_t:
-#         print(chosen_t)
-
-# transport()
-
-# trip_features = place(), food(), entertainment(), transport()
-
-# trip_features
+def project_greeting():
+    message = "Armando's Day Trip Generator"
+    print(message)
 
 def options_list():
     destinations = ["Gondor", "The Death Star", "Jurassic Park", "Willy Wonka's Chocolate Factory", 
         "Hogwarts School of Witchcraft & Wizadry", "Stanly Hotel", "King's Landing", "Gotham City"]
-    chosen_d = random.choice(destinations)
-    destination = ""
-    if destination in chosen_d:
-            print(chosen_d)
-    restaurants = ["Bob's Burgers", "The Three BroomSticks", "The Mos Eisley Cantina", "The Krusty Crab",
+    restaraunts = ["Bob's Burgers", "The Three BroomSticks", "The Mos Eisley Cantina", "The Krusty Crab",
      "Pizza Planet", "La Ratatouille", "Los Pollos Hermanos", "Gus's Galaxy Grill"]
-    chosen_r = random.choice(restaurants)
-    restaraunt = ""
-    if restaraunt in chosen_r:
-        print(chosen_r)
-    entertainment_opt = ["Learning Magic", "Running for your life", "Battling Jedi's", "Ruling a Kingdom",
+    entertainment = ["Learning Magic", "Running for your life", "Battling Jedi's", "Ruling a Kingdom",
     "Becoming a hero", "Hunting big lizards", "Carnival Ride", "Live Crowning event of new King"]
-    chosen_e = random.choice(entertainment_opt)
-    entertainments = ""
-    if entertainments in chosen_e:
-        print(chosen_e)
-    mode_of_transport = ["Flying on giant Eagles", "Millenium Falcon flight", "Jeep", "River Boat",
+    mode_of_transport = ["Flying on Giant Eagles", "Millenium Falcon flight", "Jeep", "River Boat",
     "Broomstick", "Teleportation", "Horseback drawn carriage", "Batmobile"]
-    chosen_t = random.choice(mode_of_transport)
-    transportation = ""
-    if transportation in chosen_t:
-        print(chosen_t)
-options_list() 
+
+    place = "Destination: " + random.choice(destinations)
+    food = "Restaraunt: " + random.choice(restaraunts)
+    activity = "Entertainment: " + random.choice(entertainment)
+    transport = "Transportation: " + random.choice(mode_of_transport)
+
+    return [place, food, activity, transport]
+
+def display_options(options):
+    print()
+    for option in options:
+        print(option)
+    
+def satisfaction(trip):
+    print()
+    while True:
+        prompt = input("Are the details of the trip acceptable? Y or N: ")
+        if prompt != "Y":
+            final_trip = "Here are your trip details! "
+            print()
+            print(final_trip)
+            trip = options_list()
+            display_options(trip)
+            print()
+        else:  
+            final_trip = "Here are your final trip details! "
+            print()
+            print(final_trip)
+            display_options(trip)
+            print()
+            break
+
+def main():
+    project_greeting()
+    options = options_list()
+    display_options(options)
+    satisfaction(options)
+
+main()
